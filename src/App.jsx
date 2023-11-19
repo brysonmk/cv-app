b96bea6009197adfd361b5d510a1ecb9496c4256
brysonmk <brysonkushner@gmail.com>
2023-11-19 18:28:18 -0500

    Set up project.

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,76 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState('')
+  const [name, setName] = useState('')
+  const [generalInfo, setGeneralInfo] = useState('')
+  const [education, setEducation] = useState('')
+
+
+  function handleNameChange(event){
+    setName(event.target.value)
+  }
+
+  function handleTitleChange(event){
+    setTitle(event.target.value)
+  }
+
+  function handleGeneralInfoChange(event){
+    setGeneralInfo(event.target.value)
+  }
+
+  function handleEducationChange(event){
+    setEducation(event.target.value)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <header>
+        <h1>CV Application</h1>
+      </header>
+      
+      <main>
+        <div className='input-section'>
+          <div className='title-input-form'>
+            <label>Title: 
+              <input type='text' id='title' value={title} onChange={handleTitleChange}/>
+            </label>
+          </div>
+
+          <div className='name-input-form'>
+            <label>Name: 
+              <input 
+                type="text" 
+                id="name" 
+                value={name}
+                onChange={handleNameChange}
+              />
+            </label>
+          </div>
+
+          <div className='general-info-form'>
+            <label>General Info: 
+              <input type='text' id='general-info' value={generalInfo} onChange={handleGeneralInfoChange}/>
+            </label>
+          </div>
+
+          <div className='education-form'>
+            <label>Education: 
+              <input type='text' id='education' value={education} onChange={handleEducationChange}/>
+            </label>
+          </div>
+        </div>
+
+        <div className='output-section'>
+          <h2>Title: {title}</h2>
+          <p>Name: {name}</p>
+          <p>General Info: {generalInfo}</p>
+          <p>Education: {education}</p>
+        </div>
+      </main>
+    </div>
   )
 }
 
